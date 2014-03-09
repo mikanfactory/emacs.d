c174bad8428d12e21c6ea0de257be2f47660e9fe
mikanfactory <k952i4j14x17@gmail.com>
2014-03-09 13:13:45 +0900

    Refactor init.el

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; ------------------------------------------------------------------------
+;; @ general
+
 ;;; *.~ とかのバックアップファイルを作らない
 (setq make-backup-files nil)
 ;;; .#* とかのバックアップファイルを作らない
@@ -8,7 +11,43 @@
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (add-to-list 'load-path "~/.emacs.d/elisp/yasnippet")
 
-;; キーバインドの設定
+;; 画面の設定
+(add-to-list 'default-frame-alist '(foreground-color . "brack"))
+(require 'color-theme)
+(color-theme-initialize)
+(setq custom-theme-load-path nil)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(setq molokai-theme-kit t)
+
+(global-linum-mode t)                   ;; 行番号を常に表示する
+(setq-default tab-width 2)              ;; インデントの深さを2にする
+(setq-default indent-tabs-mode nil)     ;; タブをスペースで扱う
+
+;; 行末の空白を強調表示
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "#b14770")
+
+;; 文字コードの指定
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+
+;; yes or noをy or n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+
+;; ファイルが #! から始まる場合、+xを付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+;; ------------------------------------------------------------------------
+;; @ modeline
+
+
+;; ------------------------------------------------------------------------
+;; @ key bind
+
+
 (global-set-key (kbd "C-m") 'newline-and-indent)
 (keyboard-translate ?\C-h ?\C-?)
 (define-key global-map (kbd "C-x ?") 'help-command)
@@ -21,28 +60,8 @@
 ;; EmacsではC-g C-/ でredoができる
 ;; (define-key global-map (kbd "C-.") 'redo)
 
-
-;; 画面の設定
-(add-to-list 'default-frame-alist '(foreground-color . "brack"))
-(require 'color-theme)
-(color-theme-initialize)
-(setq custom-theme-load-path nil)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(setq molokai-theme-kit t)
-
-;; 行番号を常に表示する
-(global-linum-mode t)
-
-
-;; 文字コードの指定
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8)
-
-
-;; ファイルが #! から始まる場合、+xを付けて保存する
-(add-hook 'after-save-hook
-          'executable-make-buffer-file-executable-if-script-p)
-
+;; ------------------------------------------------------------------------
+;; @ elip
 
 ;; auto-completeの設定
 (when (require 'auto-complete-config nil t)
@@ -59,19 +78,19 @@
 
 ;; auto-installの設定
 ;; ちょっと重いので、普段は外しておく
-;; (when (require 'auto-install nil t)	
-  ;; インストールディレクトリを設定する
-  ;; 初期値は ~/.emacs.d/auto-install/
-  ;; (setq auto-install-directory "~/.emacs.d/elisp")
-  
-  ;; EmacsWiki に登録されている elisp の名前を取得する
-  ;; (auto-install-update-emacswiki-package-name t)
+;; (when (require 'auto-install nil t)
+;;   ;; インストールディレクトリを設定する
+;;   ;; 初期値は ~/.emacs.d/auto-install/
+;;   (setq auto-install-directory "~/.emacs.d/elisp")
 
-  ;; 必要であればプロキシの設定を行う
-  ;; (setq url-proxy-services '(("http" . "localhost:8080")))
+;;   ;; EmacsWiki に登録されている elisp の名前を取得する
+;;   (auto-install-update-emacswiki-package-name t)
 
-  ;; install-elisp の関数を利用可能にする
-  ;; (auto-install-compatibility-setup))
+;;   ;; 必要であればプロキシの設定を行う
+;;   ;; (setq url-proxy-services '(("http" . "localhost:8080")))
+
+;;   ;; install-elisp の関数を利用可能にする
+;;   (auto-install-compatibility-setup))
 
 ;; MELPA、Marmaladeの設定
 ;; package.elはEmacs24に標準で入っている
@@ -111,3 +130,4 @@
 
 ;; Haml-mode
 (require 'haml-mode)
+
