a100176274618bca4a93bd7ec00449a2d3239453
mikanfactory <k952i4j14x17@gmail.com>
2015-02-28 23:23:08 +0900

    init.el のバグを修正

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 (global-set-key (kbd "C-m") 'newline-and-indent)
 
 ;; ----------------------------------------------------------------
+;; 設定
 ;; ----------------------------------------------------------------
 ;; rbenvで入れたrubyを使う
 (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:"
@@ -308,8 +309,6 @@
 ;; ファイルを開いたら自動的にtempbufを有効にする
 (add-hook 'find-file-hooks 'turn-on-tempbuf-mode)
 (add-hook 'dired-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'magit-status-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'magit-process-mode-hook 'turn-on-tempbuf-mode)
 (add-hook 'ag-mode-hook 'turn-on-tempbuf-mode)
 
 ;; ----------------------------------------------------------------
@@ -321,7 +320,40 @@
 
 ;; ----------------------------------------------------------------
 ;; @ helm, helm-ag, helm-c-yasnippet, hel(setq helm-buffer-max-length 50)
+;; ----------------------------------------------------------------
+(add-to-list 'load-path "~/.emacs.d/elisp/helm")
+(add-to-list 'load-path "~/.emacs.d/elisp/helm/helm-ag")
+(add-to-list 'load-path "~/.emacs.d/elisp/helm/helm-flycheck")
+(add-to-list 'load-path "~/.emacs.d/elisp/helm/helm-c-yasnippet")
 
+(require 'helm-config)
+(require 'helm-ls-git)
+(require 'helm-ag)
+(require 'helm-c-yasnippet)
+
+(define-key global-map (kbd "M-x")     'helm-M-x)
+(define-key global-map (kbd "C-, u b")   'helm-for-files)
+(define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+(define-key global-map (kbd "C-, u f") 'helm-ls-git-ls)
+
+;; Emulate `kill-line' in helm minibuffer
+(setq helm-delete-minibuffer-contents-from-point t)
+(defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+  "Emulate `kill-line' in helm minibuffer"
+  (kill-new (buffer-substring (point) (field-end))))
+
+(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+  "Execute command only if CANDIDATE exists"
+  (when (file-exists-p candidate)
+    ad-do-it))
+;; For find-file etc.
+(define-key helm-read-file-map (kbd "TAB")
+  'helm-execute-persistent-action)
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB")
+  'helm-execute-persistent-action)
+
+(setq helm-buffer-max-length 50)
 (global-set-key (kbd "C-M-z") 'helm-resume)
 
 ;; ----------------------------------------------------------------
