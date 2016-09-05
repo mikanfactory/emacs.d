d6ec108e84e58b1dfabb9335433afa1d58be8e82
mikanfactory <k952i4j14x17@gmail.com>
2016-09-05 21:15:15 +0900

    [fix] use flake8 for python lint

--- inits/20-python.el
+++ inits/20-python.el
@@ -1,6 +1,8 @@
 (setq python-indent-offset 4)
-(setq jedi:complete-on-dot t)  
+(setq jedi:complete-on-dot t)
 
 (require 'company-jedi)
 (add-to-list 'company-backends 'company-jedi)
+
+(setq flycheck-checkers '(python-flake8))
 (add-hook 'python-mode-hook 'flycheck-mode)
